ba6b0c53bf186f8487ae5bffcd2654d84ce7d614
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2026-02-04 14:13:08 +0530

    push

--- Server/src/server.js
+++ Server/src/server.js
@@ -5,7 +5,7 @@ import { Server } from 'socket.io';
 import expressApp from './app.js';
 import registerBidsocket from './socket/bid.scocket.js';
 const app = http.createServer(expressApp);
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const io = new Server(app, {
     cors: {
         origin: '*'
